1bcd072c15c70f6b1d16b993126e301abbcb7155
JobGamesJG <88381253+JobGamesJG@users.noreply.github.com>
2021-11-19 15:37:21 +0100

    feat: add about & new navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,21 +1,23 @@
-import Image from "next/image";
+import React, { useEffect, useState } from "react";
 import NavLink from "./NavLink";
 
 const Navbar: React.FC = () => {
+	const [active, setActive] = useState(false);
+	useEffect(() => window.addEventListener("scroll", () => setActive(window.scrollY > 20)), []);
+
 	return (
 		<div className="navbar-wrapper">
-			<div className="navbar-content">
-				<div style={{ display: "flex" }}>
+			<div className={active ? "navbar active" : "navbar"}>
+				<div className="navbar-content">
 					<div className="navbar-logo">
-						<Image className="navbar-img" src="/favicon.ico" alt="" width={50} height={50} />
+						<img className="navbar-img" src="https://cdn.jobgamesjg.tk/S4M2_7wt/pfp.gif" alt="" />
+						<p className="navbar-logo-text">JobGamesJG</p>
 					</div>
-					<NavLink href="/">
-						<a className="navlink-text">JobGamesJG</a>
-					</NavLink>
-				</div>
-				<div className="navbar-links">
-					<NavLink href="/">Home</NavLink>
-					<NavLink href="/about">About</NavLink>
+					<div className="navbar-routes">
+						<NavLink href="/">Home</NavLink>
+						<NavLink href="/about">About</NavLink>
+						<NavLink href="/minecraft">Minecraft</NavLink>
+					</div>
 				</div>
 			</div>
 		</div>
