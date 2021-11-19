e856f6dd0d74407b1f75970cfdc95fdc84c5f084
DaanGamesDG <daan@daangamesdg.xyz>
2021-11-19 16:02:53 +0100

    feat: add auto age

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { calculateAge } from "../lib";
 
 const Home: NextPage = () => {
+	const age = calculateAge();
+
 	return (
 		<div className="landing-wrapper">
 			<div className="landing">
 				<div className="landing-text">
 					<h1 className="landing-title">Hello I am Job 👋</h1>
-					<p className="landing-small">I am a 13 year-old java script developer</p>
+					<p className="landing-small">I am a {age} year-old programmer</p>
 				</div>
 			</div>
 		</div>
