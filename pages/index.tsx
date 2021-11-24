ca86a1e92ab6ab01d34662adf0d13115bf2e8234
JobGamesJG <88381253+JobGamesJG@users.noreply.github.com>
2021-11-24 23:01:28 +0100

    Feet: new website icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,16 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
 	return (
-		<div className="landing-wrapper">
-			<div className="landing">
-				<div className="landing-text">
-					<h1 className="landing-title">Hello I am Job 👋</h1>
-					<p className="landing-small">I am a 13 year-old java script developer</p>
+		<>
+			<div className="landing-wrapper">
+				<div className="landing">
+					<div className="landing-text">
+						<h1 className="landing-title">Hello I am Job 👋</h1>
+						<p className="landing-small">I am a 13 year-old java script developer</p>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
