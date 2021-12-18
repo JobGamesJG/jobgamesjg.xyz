8f204d9463ff6f0219b07c8f89d9002033be662f
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-18 12:44:12 +0100

    feat: add /bedankt-bezorger redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  redirects() {
+		return [
+			{
+				source: "/bedankt-bezorger",
+				destination: "https://tikkie.me/pay/sqql5191j37oi3kog5rd?",
+				permanent: false,
+			},
+		];
+	},
 }
