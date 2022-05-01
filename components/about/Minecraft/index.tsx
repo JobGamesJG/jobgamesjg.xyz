ea3efb7adfe0b32dad2b14ac22b7fb09297365fe
JobGamesJG <88381253+JobGamesJG@users.noreply.github.com>
2022-05-02 00:46:14 +0200

    feat: website deployment fix

--- components/about/Minecraft/index.tsx
+++
@@ -1,5 +0,0 @@
-import type { MojangList, MojangListRaw } from "../../../lib";
-
-export const Minecraft: React.FC<{ Mojangs: MojangListRaw[] | null }> = ({ Mojangs }) => {
-	return <div></div>;
-};
